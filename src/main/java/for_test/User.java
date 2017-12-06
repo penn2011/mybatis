720c70010f27536d091c4fe04973b7bf77a30e48
penn2011 <penn2011@126.com>
2017-12-06 21:22:54 +0800

    PO中一定要加无参数的构造器

--- src/main/java/for_test/User.java
+++ src/main/java/for_test/User.java
@@ -5,6 +5,15 @@ public class User {
     private String name;
     private Integer age;
 
+    public User() {
+    }
+
+    //    public User(Integer id, String name, Integer age) {
+//        this.id = id;
+//        this.name = name;
+//        this.age = age;
+//    }
+
     public User(String name, Integer age) {
         this.name = name;
         this.age = age;
